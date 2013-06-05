110f0f977eed8cb6d82f71db848b620d85f553be
fancl20 <fancl20@gmail.com>
2013-06-05 20:09:09 +0000

    add debug handler

--- db-server.lisp
+++ db-server.lisp
@@ -7,16 +7,16 @@
 (in-package db-server)
 
 (defun handler (socket-stream)
-    (let ((ret (eval (read socket-stream))))
-      (with-standard-io-syntax
-        (print ret socket-stream))))
+  (let ((ret (eval (read socket-stream))))
+    (with-standard-io-syntax
+      (print ret socket-stream))))
 
-;(defun handler (socket-stream)
-;  (let ((command (read socket-stream)))
-;    (format t "~a~%" command)
-;    (force-output t)
-;    (let ((ret (eval command)))
-;      (with-standard-io-syntax
-;        (print ret socket-stream)))))
+(defun debug-handler (socket-stream)
+  (let ((command (read socket-stream)))
+    (format t "~a~%" command)
+    (force-output t)
+    (let ((ret (eval command)))
+      (with-standard-io-syntax
+        (print ret socket-stream)))))
 
 (start-server #'handler)
