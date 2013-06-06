195fa5109b137627692563a8e171a54feae0d214
fancl20 <fancl20@gmail.com>
2013-06-06 20:00:18 +0000

    remove test file

--- test-sv.lisp
+++
@@ -1,8 +0,0 @@
-(require "server" "server.lisp")
-(in-package :sv)
-
-(defun echo (socket-stream)
-  (let ((str (read-line socket-stream)))
-  (format t "~A~%" str)
-  (format socket-stream "OK~%")))
-(start-server #'echo)
